d5d32e560aa00865d1062d306ae8e74786afeebb
grayolaRSA <gtndanga@gmail.com>
2023-07-28 22:15:05 +0200

    add api files

---
+++ api/v1/app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""simple Flask app"""
+
+from api.v1.views import app_views
+from flask import Flask
+from models import storage
+import os
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.route('/')
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.teardown_appcontext
+def close(exception):
+    """tears down the app when its closed"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    # Change the host parameter from '127.0.0.1' to '0.0.0.0'
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.environ.get('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
