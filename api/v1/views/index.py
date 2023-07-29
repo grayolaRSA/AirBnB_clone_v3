c7062a0ee141040fdf0579acfd8e74dc124ea34d
grayolaRSA <gtndanga@gmail.com>
2023-07-30 01:04:49 +0200

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,3 +7,14 @@ from flask import jsonify
 @app_views.route("/status")
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """returns number of each instance type """
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
