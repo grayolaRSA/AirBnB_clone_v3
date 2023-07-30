676274ae070130f41286539ad4bfca1d4cecc143
grayolaRSA <gtndanga@gmail.com>
2023-07-30 20:13:06 +0200

    put app file in right folder

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,41 +1,40 @@
 #!/usr/bin/python3
-"""simple Flask app"""
+"""
+API for AirBnB_clone_v3
+"""
 
-from api.v1.views import app_views
-from flask import Flask
+import os
+from flask import Flask, jsonify, Response
 from flask_cors import CORS
 from models import storage
-import os
-
+from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app)
-cors = CORS(app, resources={
-    r"/*": {
-        "origins": "0.0.0.0"
-    }
-})
-
-
-@app.route('/')
-def hello_world():
-    return "<p>Hello, World!</p>"
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def close(exception):
-    """tears down the app when its closed"""
+def teardown(self):
+    """ handles teardown """
     storage.close()
 
 
 @app.errorhandler(404)
-def not_fnd(error):
-    """ Returns 404 status JSON response"""
-    return make_response(jsonify({"error": "Not found"}), 404)
+def page_not_found(e):
+    """ handles 404 errors """
+    status = {"error": "Not found"}
+    return jsonify(status), 404
 
 
 if __name__ == '__main__':
-    # Change the host parameter from '127.0.0.1' to '0.0.0.0'
-    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.environ.get('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    try:
+        host = os.environ.get('HBNB_API_HOST')
+    except:
+        host = '0.0.0.0'
+
+    try:
+        port = os.environ.get('HBNB_API_PORT')
+    except:
+        port = '5000'
+
+    app.run(host=host, port=port)
